543c86855307dfbd035a07a64f3a2974ed15ce21
jatannoury-Tradias <j.tannoury.ext@bankhaus-scheich.de>
2023-07-14 15:32:27 +0300

    FRONT-END | ADD - Adding sign in screen

--- front-end/src/screens/SignUp.jsx
+++ front-end/src/screens/SignUp.jsx
@@ -2,22 +2,39 @@ import React, { useState } from "react";
 import { FaUserAlt } from "react-icons/fa";
 import Input from "../components/Input";
 import axiosInstance from "../tools/axios";
-
+import { Link, useNavigate } from "react-router-dom";
+import { encryptString } from "../tools/encrypt";
+import { toast } from "react-toastify";
 const SignUp = () => {
-    const [formData, setFormData] = useState({
-      firstName: "",
-      lastName: "",
-      birthday: "",
-      gender: "",
-      email: "",
-      password: "",
-    });
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    birthday: "",
+    gender: "",
+    email: "",
+    password: "",
+  });
   const handleSubmit = (e) => {
     e.preventDefault();
-    axiosInstance.register(formData)
+    formData["password"] = encryptString(
+      formData["password"],
+      "abcdefghijklmnop"
+    );
+    axiosInstance.register(formData).then((res) => {
+      console.log("RES",res)
+      if (res === 201) {
+        toast.success("Invalid Inputs");
+        setTimeout(() => {
+          navigate("/signIn");
+        }, 1000);
+      } else {
+        console.log("TOAST");
+        toast.error("Invalid Inputs");
+      }
+    });
   };
-  
-  
+
   return (
     <div className="signup_container">
       <form onSubmit={handleSubmit} className="form_inputs">
@@ -61,9 +78,12 @@ const SignUp = () => {
             formData={formData}
           />
         </div>
-        <div className="input_component">
+        <div className="input_component submit sign_up">
           <input type="submit"></input>
         </div>
+        <Link to={"/signIn"} className="link_to_sign_in ">
+          <span>Already a User? Sign In</span>
+        </Link>
       </form>
     </div>
   );
