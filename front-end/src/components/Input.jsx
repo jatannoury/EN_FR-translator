ad353d016ba97e82c8edb27db76714ed468da404
jatannoury-Tradias <j.tannoury.ext@bankhaus-scheich.de>
2023-07-14 11:23:01 +0300

    FRONT-END | ADD - Adding user to database

--- front-end/src/components/Input.jsx
+++ front-end/src/components/Input.jsx
@@ -1,43 +1,62 @@
-import React from 'react'
+import React from "react";
 
-const Input = ({ placeholder, setFormData,formData }) => {
+const Input = ({ placeholder, setFormData, formData }) => {
   let placeholder_id = placeholder.replace(" ", "");
   placeholder_id =
     placeholder_id.charAt(0).toLowerCase() + placeholder_id.slice(1);
+
   const handleInputChange = (e) => {
     setFormData((prevFormData) => ({
       ...prevFormData,
-      [[placeholder_id]]: e.target.value.toString(),
+      [placeholder_id]: e.target.value.toString(),
     }));
   };
-  
 
   return (
     <div className="input_component">
-      <label htmlFor="input_component">{placeholder}</label>
-      {placeholder !== "Birthday" && placeholder !== "Gender" ? (
+      <label htmlFor={placeholder_id}>{placeholder}</label>
+      {placeholder === "Password" ? (
+        <input
+          type="password"
+          placeholder={placeholder}
+          onChange={handleInputChange}
+          id={placeholder_id}
+        />
+      ) : placeholder === "Birthday" ? (
+        <input type="date" className="birthday" onChange={handleInputChange} />
+      ) : placeholder === "Gender" ? (
+        <div className="radio-container">
+          <input
+            type="radio"
+            id="male"
+            name="gender"
+            value="male"
+            onChange={handleInputChange}
+          />
+          <label htmlFor="male" className="gender_label">
+            Male
+          </label>
+          <input
+            type="radio"
+            id="female"
+            name="gender"
+            value="female"
+            onChange={handleInputChange}
+          />
+          <label htmlFor="female" className="gender_label">
+            Female
+          </label>
+        </div>
+      ) : (
         <input
           type="text"
           placeholder={placeholder}
           onChange={handleInputChange}
           id={placeholder_id}
-        ></input>
-      ) : placeholder === "Birthday" ? (
-        <input type="date" className="birthday" onChange={handleInputChange}/>
-      ) : (
-        <div className="radio-container">
-          <input type="radio" id="male" name="gender" value="male" onChange={handleInputChange}/>
-          <label for="male" className="gender_label">
-            Male
-          </label>
-          <input type="radio" id="female" name="gender" value="female" onChange={handleInputChange}/>
-          <label for="female" className="gender_label">
-            Female
-          </label>
-        </div>
+        />
       )}
     </div>
   );
 };
 
-export default Input
+export default Input;
