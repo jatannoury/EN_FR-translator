ff32433e60ee24bdc4f89a4114af2d9e17edbc04
jatannoury-Tradias <j.tannoury.ext@bankhaus-scheich.de>
2023-07-16 01:21:25 +0300

    BACK_END | FIX - Fixing bug in save history

--- back-end/routes/translation.py
+++ back-end/routes/translation.py
@@ -1,7 +1,6 @@
-from http.client import HTTPException
 
 from fastapi import  APIRouter,Query
-
+from fastapi.responses import Response
 from models.ModelInput import ModelInput
 from models.history import History
 from tools.dynamoDB import DynamoDbHandler
@@ -17,15 +16,27 @@ def translate(data:History):
     try:
         history_data = data.__dict__
         translated_sentence = model.translate(history_data['en_text'])
-        translation_id = dynamoDB_handler.add_history(data)
+        translation_id = dynamoDB_handler.add_history(data,translated_sentence)
         return {
             "translation_id":translation_id,
             "translated_sentence":translated_sentence
         }
 
     except:
-        raise HTTPException(status_code=500, detail="Internal Server Error")
+        return Response(status_code=500,content="Server Error")
 
 
+
+@router.post("/save_translation", status_code=200)
+def save_translation(translation_id:str = Query(..., description="translation_id")):
+    try:
+        status_code, save_status= dynamoDB_handler.save_translation_id(translation_id)
+        if status_code==200:
+            return {"message":f"{'Saved' if save_status == 1 else 'Removed'}"} ,200
+        else:
+            return Response(status_code=500)
+
+    except:
+        return Response(status_code=500)
 
 
--- back-end/tools/dynamoDB.py
+++ back-end/tools/dynamoDB.py
@@ -11,26 +11,28 @@ from tools.decrypt import aes_decrypt_string
 
 load_dotenv()
 
+
 class DynamoDbHandler:
     def __init__(self):
         self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
         dynamo_db_client = boto3.resource(
             'dynamodb',
-            aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID"),
-            aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
+            aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY")
         )
         self.users_table = dynamo_db_client.Table("users")
         self.history_table = dynamo_db_client.Table("history")
+
     # Password encryption function
-    def encrypt_password(self,password: str) -> str:
+    def encrypt_password(self, password: str) -> str:
         encrypted_pass = self.pwd_context.hash(password)
         return encrypted_pass
 
-    def verify_password(self,password: str, hashed_password: str) -> bool:
+    def verify_password(self, password: str, hashed_password: str) -> bool:
         pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
         return pwd_context.verify(password, hashed_password)
 
-    def post_user_table(self,formData) -> str:
+    def post_user_table(self, formData) -> str:
         try:
             user_data = formData.__dict__
             user_data['userId'] = str(uuid.uuid4())
@@ -41,7 +43,7 @@ class DynamoDbHandler:
             print(e)
             raise HTTPException(status_code=500, detail="Internal Server Error")
 
-    def get_user_info(self,user_email) -> str:
+    def get_user_info(self, user_email) -> str:
         try:
             filter_expression = 'email = :user_email'
             expression_attribute_values = {
@@ -57,9 +59,55 @@ class DynamoDbHandler:
             print(e)
             raise HTTPException(status_code=500, detail="Internal Server Error")
 
-    def add_history(self,data:History):
+    def add_history(self, data: History,translated_sentence:str):
         history_info = data.__dict__
         history_info['saved'] = 0
         history_info['translation_id'] = str(uuid.uuid4())
+        history_info['fr_text'] = translated_sentence
         self.history_table.put_item(Item=history_info)
         return history_info['translation_id']
+
+    def query_history_table(self, translation_id):
+        filter_expression = 'translation_id = :translation_id'
+        expression_attribute_values = {
+            ":translation_id": translation_id
+        }
+        print(translation_id)
+        return self.history_table.scan(
+            FilterExpression=filter_expression,
+            ExpressionAttributeValues=expression_attribute_values
+        )
+
+    def save_translation_id(self, translation_id):
+        """Saves the translation ID to the history table.
+
+        Args:
+            translation_id (str): The ID of the translation to be saved.
+        """
+        translation_id = str(translation_id)
+
+        translation_info = self.query_history_table(translation_id)['Items'][0]
+
+        item_key = {
+            'translation_id': translation_id,
+            "user_id":translation_info['user_id']
+        }
+
+        translation_info['saved'] = 0 if int(translation_info['saved'] )== 1 else 1
+
+
+        # Update the item in the DynamoDB table.
+        response = self.history_table.update_item(
+            Key=item_key,
+            UpdateExpression='SET saved = :saved',
+            ExpressionAttributeValues={':saved': translation_info['saved']}
+        )
+        print(response['ResponseMetadata']['HTTPStatusCode'] )
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            print('Translation ID saved successfully.')
+            return 200 , translation_info['saved']
+        else:
+            return response['ResponseMetadata']['HTTPStatusCode'], None
+
+
+
